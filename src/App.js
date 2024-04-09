ace918ae093a301536e2a1b3e34a62acc4199f7f
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-04-09 12:26:43 +0530

    Initaial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import newLogo from "./assets/logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Navbar;
