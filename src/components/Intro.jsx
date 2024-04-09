46f6da996598632ad73c17ce6cf6a290353b0e2f
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-04-09 13:18:22 +0530

    intro and navbar is completed

---
+++ src/components/Intro.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assets/intro-logo.svg";
+
+const Intro = () => {
+  return (
+    <div className="flex justify-around mt-[6%] items-center relative">
+      <img src={logo} className="absolute intro-logo" alt="logo" />
+      <div className="m-0">
+        <h1 className="text-[#181717] text-6xl w-[40%] font-medium leading-[76px] ml-16">
+          Hello, I'm Andres Gallardo
+        </h1>
+      </div>
+      <div className="">
+        <p className="text-3xl text-[#606060] w-[508px] leading-[37px] mr-4 font-normal">
+          Unleash Creativity, Capture Vision:
+        </p>
+        <p className="text-3xl text-[#606060] w-[400px] leading-[37px] mr-4 font-normal">
+          Your Journey Through My Portfolio Begins Here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import logo from "../assets/logo.svg";
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex justify-between mr-6 items-center">
       <img src={logo} alt="logo" />
+      <div className="flex gap-8 text-2xl font-semibold">
+        <a href="/">Work</a>
+        <a href="/">Platform</a>
+        <a href="/">Profile</a>
+      </div>
     </div>
   );
 };
